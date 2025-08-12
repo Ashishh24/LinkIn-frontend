51102cedb80d9eec6c6b563a4d232aedd32f9f67
Ashish Anand <aashishanand07@gmail.com>
2025-08-12 23:22:36 +0530

    minor updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ const Header = () => {
     if (!user) return null;
 
     return (
-        <div className="flex h-20 items-center justify-between m-auto dark:bg-gray-950 dark:text-white">
+        <div className="flex h-20 items-center justify-between m-auto">
             <div className="w-[60%] flex">
                 <div className="w-[33%]">
                     <Link to="/home"><img className="w-15 mx-auto" src={logo} ></img></Link>
@@ -60,19 +60,17 @@ const Header = () => {
                     <form  onSubmit={handleSearch} className="max-w-md mx-auto w-full ">   
                         <div className="relative">
                             <div className="absolute inset-y-0 flex items-center ps-3 ">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                             </div>
-                            <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} className=" w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                            <button type="submit" className="hidden sm:block text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-[#2E78B6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                            <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} className=" w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." />
+                            <button type="submit" className="hidden sm:block text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-[#2E78B6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
 
                             {/* Icon Button for mobile */}
                             <button
                                 type="submit"
-                                className="sm:hidden absolute end-2.5 bottom-2.5 p-2 rounded-lg text-white bg-blue-700 
-                                        hover:bg-[#2E78B6] focus:ring-4 focus:outline-none focus:ring-blue-300 
-                                        dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                className="sm:hidden absolute end-2.5 bottom-2.5 p-2 rounded-lg text-white bg-blue-700 hover:bg-[#2E78B6] focus:ring-4 focus:outline-none focus:ring-blue-300"
                             >
                                 <svg
                                 className="w-5 h-5"
