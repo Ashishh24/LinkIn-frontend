caf46094a3b9ef44e9811fad313bc509039e9b2f
Ashish Anand <aashishanand07@gmail.com>
2025-08-13 13:29:27 +0530

    Updated ViewProfile

--- src/components/ViewProfile.js
+++ src/components/ViewProfile.js
@@ -48,17 +48,22 @@ const ViewProfile = () => {
                         <img src={userData.profilePhoto} className="rounded-full w-48 h-48 mx-auto mb-4 border-4 border-indigo-800 dark:border-blue-900 transition-transform duration-300 hover:scale-105"/>
                         <h1 className="text-2xl font-bold text-indigo-800 dark:text-white mb-2">{userData.firstName + " " + userData.lastName}</h1>
                         <p className="text-gray-600 dark:text-gray-300">{userData.title}</p>
-                        {isPendingReq ? (<div className="flex justify-center gap-5">
-                            <button 
-                                onClick={() => handleConnect(_id,"accept")} 
-                                className="mt-4 bg-indigo-800 cursor-pointer text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
-                                Accept
-                            </button><button 
-                                onClick={() => handleConnect(_id,"reject")} 
-                                className="mt-4 bg-indigo-800 cursor-pointer text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
-                                Reject
-                            </button>
-                        </div>) : null}
+                        <div className="flex justify-center gap-5">
+                            {isPendingReq  
+                                ? (<div>
+                                    <button 
+                                        onClick={() => handleConnect(_id,"accept")} 
+                                        className="mt-4 bg-indigo-800 cursor-pointer text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
+                                        Accept
+                                    </button><button 
+                                        onClick={() => handleConnect(_id,"reject")} 
+                                        className="mt-4 bg-indigo-800 cursor-pointer text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
+                                        Reject
+                                    </button>
+                                </div>)
+                                : null
+                            }
+                        </div>
                     </div>
 
                     {/* Right Section */}
