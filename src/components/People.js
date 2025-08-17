6271dfe2be03d43e780a93297029c9d8bd6ce6e9
Ashish Anand <aashishanand07@gmail.com>
2025-08-18 00:08:34 +0530

    chat started

--- src/components/People.js
+++ src/components/People.js
@@ -44,10 +44,15 @@ const People = ({ data, type }) => {
                 <div className="w-[100%] flex flex-col sm:flex-row sm:items-end justify-end gap-4 pt-2">
                     <div>
                         {type === "connection" && (
-                            <div>
+                            <div className="flex gap-5">
                                 <button className="bg-gray-400 text-white px-5 py-2 rounded-3xl" disabled>
                                     Friends
                                 </button>
+                                <Link to="/chat/:toUserId">
+                                    <button className="bg-green-400 text-white px-5 py-2 rounded-3xl cursor-pointer hover:bg-green-700">
+                                        Chat
+                                    </button>
+                                </Link>
                             </div>
                         )}
                         {type === "request" && (
